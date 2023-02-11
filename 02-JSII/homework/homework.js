44414a8f18b8def0d655ce5a12274fbe601073b5
merlin-2016 <daverano@yahoo.com>
2023-02-11 20:23:37 -0300

    estudiando right now

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -43,7 +43,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    if (elemento == array[i])
+    return true
 
+  }
+    return false
 }
 
 function promedioResultadosTest(resultadosTest) {
