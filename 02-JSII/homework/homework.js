69a36286160d28268e581658d7a28fa38f14313f
merlin-2016 <daverano@yahoo.com>
2023-03-14 23:01:21 -0300

    otra vez volvi

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -128,7 +128,20 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   // Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-
+  var algunosMeses = []
+  
+    for (let i = 0; i < array.length; i++) {
+      if( array[i] == "Enero" && array[i] == "Marzo" && array[i] == "Noviembre"){
+        array1 = ("Enero", "Marzo", "Noviembre");
+      return algunosMeses;
+      }
+        else {
+      return "No se encontraron los meses pedidos";
+    }
+      
+         
+      
+    }
 }
 
 function breakStatement(numero) {
